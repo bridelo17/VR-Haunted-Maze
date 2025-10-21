55079c8eb8a39871e7d399bbe1b98c3eeb29aaef
bridelo17 <bridelo17@gmail.com>
2025-10-20 23:19:45 -0400

    Updates
    Adding Win/Loss Condition
    Fixing Navmesh missing bug

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -5,12 +5,18 @@ public class player : MonoBehaviour
 {
      
     public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public float life = 1;
     private int count;
+    
 
     void Start()
     {
     count = 0;
-    SetCountText();   
+    SetCountText();  
+    winTextObject.SetActive(false);
+    loseTextObject.SetActive(false); 
     }
     void OnTriggerEnter(Collider other)
     {
@@ -20,14 +26,27 @@ public class player : MonoBehaviour
             count = count + 1;
             SetCountText();
         }
+    
     }
     void SetCountText()
     {
         countText.text = "Pumpkin Count: " + count.ToString();
+        if (count>=4)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+    private void OnCollisionEnter(Collision collision) 
+    {
+        if (collision.gameObject.CompareTag("Ghost"))
+        {
+            
+            life =- 1;
+        }
+        if (life <=0)
+        {
+            loseTextObject.SetActive(true);
+        }
     }
 
-    void Update()
-    {
-        
-    }
 }
